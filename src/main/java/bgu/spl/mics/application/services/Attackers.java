5f9e47de7188e6fdb08c01f84746d3e2dcd6e25a
Sapir Anidgar <sapirani@post.bgu.ac.il>
2020-12-13 22:38:54 +0200

    adding notes to Attackers - not done
    I love Sagi so much i can't explain how much <3

--- src/main/java/bgu/spl/mics/application/services/Attackers.java
+++ src/main/java/bgu/spl/mics/application/services/Attackers.java
@@ -1,23 +1,33 @@
-package bgu.spl.mics.application.services;
+package bgu.spl.mics.application.services; // The package
 
-import bgu.spl.mics.Callback;
+// Imports:
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
 
+/**
+ * The Class is the parent class of HanSolo and C3PO
+ * Used to prevent duplicate code for the two threads.
+ */
 public abstract class Attackers extends MicroService
 {
     /**
+     * The Class's Constructor
      * @param name the micro-service name (used mainly for debugging purposes -
      *             does not have to be unique)
      */
-    public Attackers(String name) {
+    public Attackers(String name)
+    {
         super(name);
     }
 
     @Override
-    protected void initialize() {
+    /**
+     * this method is called once when the event loop starts.
+     */
+    protected void initialize()
+    {
         subscribeEvent(AttackEvent.class, (attackMsg)->
         {
             try
