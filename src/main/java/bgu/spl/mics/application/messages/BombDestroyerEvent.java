3265a8b38ba02ae49212d7d711c24704a94563d5
Sapir Anidgar <sapirani@post.bgu.ac.il>
2020-12-13 21:06:02 +0200

    adding notes to all the Messages in Application

--- src/main/java/bgu/spl/mics/application/messages/BombDestroyerEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/BombDestroyerEvent.java
@@ -1,7 +1,10 @@
-package bgu.spl.mics.application.messages;
+package bgu.spl.mics.application.messages; // The package
 
+// Imports:
 import bgu.spl.mics.Event;
 
-public class BombDestroyerEvent implements Event<Boolean>
-{
-}
+/**
+ * Class that represents Bomb Destroyer event
+ * Received only by Lando after R2D2 finishes deactivating the shield generator.
+ */
+public class BombDestroyerEvent implements Event<Boolean> { }
